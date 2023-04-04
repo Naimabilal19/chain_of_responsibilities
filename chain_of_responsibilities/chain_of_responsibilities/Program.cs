d972379bc35a142f8e1a553ec0701363bf116e90
Наима <113019779+Naimabilal19@users.noreply.github.com>
2023-04-05 00:51:38 +0300

    Add files via upload

---
+++ chain_of_responsibilities/chain_of_responsibilities/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chain_of_responsibilities
+{
+    interface IReceiver
+    {
+        IReceiver SetNext(IReceiver receiver);
+        object Handle(object request);
+    }
+
+    abstract class PaymentHandler : IReceiver
+    {
+        private IReceiver next_handler;
+
+        public IReceiver SetNext(IReceiver receiver)
+        {
+            next_handler = receiver;
+            return receiver;
+        }
+        public virtual object Handle(object request)
+        {
+            if (this.next_handler != null)
+            {
+                return this.next_handler.Handle(request);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+
+    class MoneyPymentHandler : PaymentHandler
+    {
+        public override object Handle(object request)
+        {
+            if (request as string == "Money")
+            {
+                return "Money payment";
+            }
+            else
+            {
+                return base.Handle(request); ;
+            }
+        }
+    }
+
+    class PayPalPaymentHandler : PaymentHandler
+    {
+        public override object Handle(object request)
+        {
+            if (request as string == "PayPal")
+            {
+                return "PayPal payment";
+            }
+            else
+            {
+                return base.Handle(request);
+            }
+        }
+    }
+
+    class BankPaymentHandler : PaymentHandler
+    {
+        public override object Handle(object request)
+        {
+            if (request as string == "Bank")
+            {
+                return "Bank payment";
+            }
+            else
+            {
+                return base.Handle(request);
+            }
+        }
+    }
+
+    class Client
+    {
+        public void ClientCode(PaymentHandler ph)
+        {
+            foreach (var payment in new List<string> { "Money", "Bank", "PayPal" })
+            {
+                Console.WriteLine($"Client: {payment}");
+                var result = ph.Handle(payment);
+                if (result != null)
+                {
+                    Console.Write($"    {result}\n");
+                }
+                else { Console.WriteLine($"   {payment} untouched."); }
+            }
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var money = new MoneyPymentHandler();
+            var paypal = new PayPalPaymentHandler();
+            var bank = new BankPaymentHandler();
+
+            money.SetNext(paypal).SetNext(bank);
+
+            Client client = new Client();
+            client.ClientCode(money);
+
+        }
+    }
+}
